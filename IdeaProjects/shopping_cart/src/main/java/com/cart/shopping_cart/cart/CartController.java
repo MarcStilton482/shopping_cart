698d9b59375af30d4d789777661b02e851e5f443
MarcStilton482 <135502520+MarcStilton482@users.noreply.github.com>
2023-07-01 17:32:13 +0530

    eighth commit

--- IdeaProjects/shopping_cart/src/main/java/com/cart/shopping_cart/cart/CartController.java
+++ IdeaProjects/shopping_cart/src/main/java/com/cart/shopping_cart/cart/CartController.java
@@ -1,10 +1,16 @@
 package com.cart.shopping_cart.cart;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import javafx.scene.control.Label;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public class CartController {
@@ -12,7 +18,7 @@ public class CartController {
     public VBox cartPane;
 
     @FXML
-    public void initialize() {
+    public void initialize() throws FileNotFoundException {
         //populate the view
         List<CartEntry> entries = ShoppingCart.getInstance().getEntries();
         cartPane.getChildren().clear();
@@ -25,11 +31,36 @@ public class CartController {
             cartPane.getChildren().add(shoppingCartTitle);
 
             for (CartEntry cartEntry : entries) {
-                HBox hBox = new HBox();
-                Label productName = new Label(cartEntry.getProduct().name());
-                hBox.getChildren().add(productName);
-                cartPane.getChildren().add(hBox);
+                HBox productView = cartEntryView(cartEntry);
+                cartPane.getChildren().add(productView);
             }
         }
+    }
+    private HBox cartEntryView(CartEntry cartEntry) throws FileNotFoundException{
+        HBox layout = new HBox();
+        layout.setAlignment(Pos.CENTER_LEFT);
+
+        FileInputStream input = new FileInputStream(
+                "C:/Users/ADMIN/IdeaProjects/shopping_cart/src/main/java/resources/"+cartEntry.getProduct().getImageFile());
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(50);
+        imageView.setFitWidth(50);
+
+        Label productName = new Label(cartEntry.getProduct().name());
+        productName.setPrefWidth(100);
+        productName.setStyle("-fx-font-size:15pt; -fx-padding:5px");
+
+        Label quantity = new Label((String.valueOf(cartEntry.getQuantity())));
+        quantity.setStyle("-fx-padding:5px");
+        Button plusButton = new Button("+");
+        plusButton.setStyle("-fx-padding:5px");
+        Button minusButton = new Button("-");
+        minusButton.setStyle("-fx-padding:5px");
+        Label price = new Label(cartEntry.getProduct().getPrice() + " Rs");
+
+        layout.getChildren().addAll(imageView, productName, plusButton, quantity, minusButton, price);
+
+        return layout;
     }
 }
